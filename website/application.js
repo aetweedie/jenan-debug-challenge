a6600121ef013162dddec4d3e314a06815a8a697
Jenan <jenanmannette@gmail.com>
2015-04-19 10:31:49 -0600

    My solutions to precourse work

---
+++ website/application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#stitch").hide().fadeIn(3000);
+});
+$(document).ready(function(){
+    $("#info").click(function(){
+      $(".list").slideToggle("slow");
+    });
+});
